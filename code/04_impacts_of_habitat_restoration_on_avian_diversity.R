83803d181be58e8e7983031121a98e2a156c8a65
Joshua Morris <joshm@birdsconnectsea.org>
2024-08-26 19:35:07 -0700

    additional work on habitat restoration impacts analysis

--- code/04_impacts_of_habitat_restoration_on_avian_diversity.R
+++ code/04_impacts_of_habitat_restoration_on_avian_diversity.R
@@ -6,25 +6,29 @@
 # Battey and Ross selected NBP point count areas within 50 meters of 
 # habitat restoration zones at Golden Gardens, Discovery, Carkeek, and Magnuson Parks.
 
-## In ArcGIS, used GSP reference map and select by location tool to 
+## In ArcGIS, used GSP reference map and select by location tool to identify NBP circles
+## adjacent to GSP restoration sites.
 
 # load packages
 library(tidyverse)
 library(readxl)
+library(lme4)
 
 # data
 ## tidied NPB data
 dat <- read_xlsx("data/b_intermediate_data/nbp_tidy_jan_24.xlsx")
 
-## count circle near GSP restoration site data
+## count circles near GSP restoration sites by phase data
 gsp <- read_csv("data/c_analysis_data/circ_gsp_phases.csv")
+
+## proportion of count circle overlapping with GSP restoration site by phase
 gsp.prop <- read_csv("data/c_analysis_data/circ_gsp_phases_prop_area.csv")
 
 ## count circle codes to help join to main data
 cod <- read_csv("data/c_analysis_data/nbp_circ_codes.csv")
 
 ## The restoration analysis in 2014 asked if there are differences in mean abundance per survey and
-## mean annual species diversity, the nuber of species reported annually per circle.
+## mean annual species diversity, the number of species reported annually per circle.
 
 ## Step 1: calculate mean abundance per survey and mean diversity per station per year
 
@@ -33,7 +37,7 @@ dres <- dat %>%
   filter(!str_detect(species, pattern = " sp."), !str_detect(species, pattern = " sp."),
          !year %in% c(1996, 2020, 2024))
 
-## create a diversity / abundance dataframe for each station for each year
+## create a diversity / abundance data frame for each station for each year
 
 ddiv <- dres %>%
   group_by(year, park, loop, station) %>%
@@ -48,7 +52,7 @@ gsp$lu <- paste0(gsp$park, gsp$loop, gsp$station)
 ddiv <- left_join(ddiv, gsp)
 
 
-## One challenge is that many or most stations are likley within 50 meters of several GSP phases. How
+## One challenge is that many or most stations are within 50 meters of several GSP phases. How
 ## many are just near one phase?
 
 ### first, replace phase data with 0, 1, 2, 3, 4, and . to make easier to read. Will want to concatenate
@@ -72,19 +76,27 @@ ddiv$near_phase_4 <- replace(ddiv$near_phase_4, ddiv$near_phase_4 == "n", ".")
 ddiv$phase_sum <- paste(ddiv$near_phase_0, ddiv$near_phase_1, ddiv$near_phase_2, ddiv$near_phase_3, ddiv$near_phase_4, sep = "")
 
 cphase <- c("0....", ".1...", "..2..", "...3.", "....4")
+focal.parks <- c("Discovery Park", "Magnuson Park", "Carkeek Park", "Golden Gardens Park")
 
+### AOV and Tukey test
 
-phases <- ddiv %>%
-  group_by(year, phase_sum) %>%
-  summarise(n = n())
+d <- filter(ddiv, park %in% focal.parks, phase_sum %in% cphase, year == 2023)
 
-view(filter(phases, year == 2022, phase_sum %in% cphase))
-filter(phases, year == 2022) %>%
-  summarise(sum = sum(n))
+phase_aov <- aov(S ~ phase_sum, data = d) 
+TukeyHSD(phase_aov)
 
-### in 2022, there are 59 circles next to just one gsp phase out of 199 
+summary(d)
 
-### in wonder if it would be better to calculate the overlap of each count circle with area of GSP phase?
+ggplot((d %>% group_by(phase_sum) %>% summarise(mean = mean(S))), aes(x = phase_sum, y = mean)) + 
+  geom_col()
+
+ggplot(d, aes(x = phase_sum, y = S)) +
+  geom_boxplot(fill = bcs_colors["dark green"], color = bcs_colors["bright green"]) + geom_jitter(color = bcs_colors["bright green"])+
+  theme_bcs()
+
+### I don't know. I don't feel very convinced by this analysis approach. 
+
+### I wonder if it would be better to calculate the overlap of each count circle with area of GSP phase?
 
 ddiv <- dres %>%
   group_by(year, park, loop, station) %>%
@@ -104,61 +116,19 @@ view(ddiv)
 
 ## so let's try a mixed effect model for 2022 where there are fixed effects for prop gsp and rand effect for park
 
-prop.mixed <- lmer(S ~ prop_0 + prop_1 + prop_2 + prop_3 + prop_4 + (1 | park), data = filter(ddiv, year == 2022))
+d <- filter(ddiv, park %in% focal.parks)
+
+## I've been playing around with values for year, so far only found significant effects for 2022
+prop.mixed <- lmer(S ~ prop_0 + prop_1 + prop_2 + prop_3 + prop_4 + (1 | park), data = filter(d, year == 2022))
 
 summary(prop.mixed)
 
-prop.mixed <- lmer(S ~ prop_4 + (1 | park), data = filter(ddiv, year == 2023))
+prop.mixed <- lmer(S ~ prop_4 + (1 | park), data = filter(ddiv, year == 2019))
 summary(prop.mixed)
 
 
-ggplot(filter(ddiv, year == 2022), aes(x = prop_4, y = S)) +
+ggplot(filter(d, year == 2022), aes(x = prop_4, y = S)) +
   geom_point(na.rm = TRUE) +
   geom_smooth(method = "lm", se = FALSE, na.rm = TRUE)
 
 
-view(ddiv %>%
-       filter(phase_sum %in% cphase) %>%
-  group_by(park, phase_sum) %>%
-  summarise(n = n()))
-
-phase_aov <- aov(meanS ~ phase_sum + year, data = df)
-
-## Not clear to me how this robustly controls for or incorporates random effects of park and year? Seems like it 
-## would be important to control for year? Keep year constant then search for differences? Also need to control
-## for park? Or allow a random effect? 
-
-### So perhpas we could build a mixed effects model with S ~ phase_sum and a random effect for park?
-library(lme4)
-
-dme <- ddiv %>%
-  filter(year == 2021, phase_sum %in% cphase) %>%
-  group_by(park, phase_sum) %>%
-  summarise(n = n(), maps = mean(maps), meanS = mean(S))
-  
-
-phase22.mixed <- lmer(meanS ~ phase_sum + (1 | park), data = dme)
-summary(phase22.mixed)
-
-ggplot(dme, aes(x = phase_sum, y = meanS)) +
-  geom_col() + 
-  facet_wrap(~ park)
-
-## so none of the parks have
-
-summary(phase_aov)
-hist(phase_aov$residuals)
-
-TukeyHSD(phase_aov)
-
-view(df)
-
-dplot <- df %>%
-  group_by(phase_sum) %>%
-  summarise(meanS = mean(meanS), maps = mean(maps))
-
-ggplot(dplot, aes(x = phase_sum, y = meanS)) +
-  geom_col()
-
-ggplot(dplot, aes(x = phase_sum, y = maps)) + 
-  geom_col()
